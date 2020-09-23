7a0a2d41aad382d28d2ace2e77e0cecf092d43a5
jiangwei1 <jiangwei1@kylinos.com>
2020-09-23 17:52:03 +0800

    完成首页显示，博客详细，新增博客功能.

--- blogin/blueprint/backend/forms.py
+++ blogin/blueprint/backend/forms.py
@@ -11,6 +11,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
 from wtforms import StringField, SelectField, TextAreaField, FileField, SubmitField
 from wtforms.validators import Length, DataRequired
+from blogin.models import BlogType
 
 
 class PostForm(FlaskForm):
@@ -18,13 +19,17 @@ class PostForm(FlaskForm):
                         render_kw={'class': '', 'rows': 50, 'placeholder': '输入您的博客标题'})
     blog_type = SelectField(label=u'博客类型',
                             default=0,
-                            coerce=int,
-                            choices=[(1, 'Python'), (2, 'PHP')])
+                            coerce=int)
     blog_level = SelectField(label=u'博客权限', choices=[(1, '公开'), (2, '私有')], validators=[DataRequired()],
                              default=1, coerce=int)
     brief_content = TextAreaField(u'博客简介', validators=[DataRequired()])
     blog_img_file = FileField(label=u'博客示例图',
-                              validators=[FileAllowed(['png', 'jpg'], '只接收png和jpg图片')],
+                              validators=[DataRequired(), FileAllowed(['png', 'jpg'], '只接收png和jpg图片')],
                               render_kw={'value': "上传", 'class': 'btn btn-default'})
-    body = CKEditorField('Body', validators=[DataRequired()])
+    body = CKEditorField('Body', validators=[DataRequired(message='请输入博客内容')])
     submit = SubmitField(u'发布博客')
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        categories = BlogType.query.all()
+        self.blog_type.choices = [(cate.id, cate.name) for cate in categories]
