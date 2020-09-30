8faf824c22470417be191d0af0de11a2c3601cdb
jiangwei1 <jiangwei1@kylinos.com>
2020-09-30 17:09:37 +0800

    完成IP真实归属地查询工具

--- blogin/blueprint/front/tool.py
+++ blogin/blueprint/front/tool.py
@@ -9,7 +9,8 @@
 from flask import Blueprint, render_template, request, jsonify, send_from_directory
 
 from blogin.setting import basedir
-from blogin.utils import allow_img_file, OCR
+from blogin.utils import allow_img_file, OCR, IPQuery, IP_REG
+import re
 
 tool_bp = Blueprint('tool_bp', __name__, url_prefix='/tool')
 
@@ -26,13 +27,29 @@ def ocr():
         filename = img.filename
         if allow_img_file(filename):
             return jsonify({'tag': 0, 'info': '请上传jpg/png格式图片!'})
-        img.save(basedir+'/uploads/ocr/'+filename)
+        img.save(basedir + '/uploads/ocr/' + filename)
         category = request.form.get('category')
-        img_url = '/tool/ocr/'+filename
-        c_ocr = OCR(filename=basedir + r'/uploads/ocr/'+filename, category=category)
+        img_url = '/tool/ocr/' + filename
+        c_ocr = OCR(filename=basedir + r'/uploads/ocr/' + filename, category=category)
         nums, texts = ocr_result(category, c_ocr)
         return jsonify({'tag': 1, 'nums': nums, 'texts': texts, 'img': img_url})
     return render_template("main/ocr.html")
+
+
+@tool_bp.route('/query-ip/', methods=['GET', 'POST'])
+def query_ip_addr():
+    if request.method == 'POST':
+        ip = request.form.get('ip')
+        lang = request.form.get('lang')
+        if not re.match(IP_REG, ip, flags=0):
+            return jsonify({'tag': 0, 'info': '请输入正确的IP地址.'})
+        ipq = IPQuery(ip=ip, lang=lang)
+        result = ipq.query()
+        region = [result[0], result[1], result[2]]
+        region = '-'.join(region)
+        return jsonify({'tag': 1, 'region': region, 'result': ipq.query()})
+
+    return render_template('main/queryIP.html')
 
 
 @tool_bp.route('/<path>/<filename>')
