d79670b756f2c673de39bae19b51be4b37620e12
jiangwei1 <jiangwei1@kylinos.com>
2020-10-13 13:57:26 +0800

    display all log files

--- blogin/blueprint/backend/other_bp.py
+++ blogin/blueprint/backend/other_bp.py
@@ -6,6 +6,7 @@
 @File    : other_bp
 @Software: PyCharm
 """
+import os
 from datetime import datetime
 
 from flask import Blueprint, render_template
@@ -24,25 +25,32 @@ def add_timeline():
 def look_logs():
     logs = []
     import os
+    app_log_path = basedir + '/logs/'
+    nginx_log_path = '/var/log/nginx/'
+
     # 运行日志
-    app_log_update_time = os.path.getmtime(basedir + '/logs/blogin.log')
-    app_log_update_time = get_file_mtime(app_log_update_time)
-    app_log_size = os.path.getsize(basedir + '/logs/blogin.log')
+    get_log_file_info(app_log_path, logs)
+    get_log_file_info(nginx_log_path, logs, log_cate='nginx access/error 文件日志!')
     # nginx 日志
-    nginx_access_log_update_time = os.path.getmtime('/var/log/nginx/access.log')
-    nginx_access_log_update_time = get_file_mtime(nginx_access_log_update_time)
-    nginx_access_log_size = os.path.getsize('/var/log/nginx/access.log')
+    # nginx_access_log_update_time = get_file_mtime('/var/log/nginx/access.log')
+    # nginx_access_log_size = os.path.getsize('/var/log/nginx/access.log')
+    #
+    # nginx_error_log_update_time = get_file_mtime('/var/log/nginx/error.log')
+    # nginx_error_log_size = os.path.getsize('/var/log/nginx/error.log')
 
-    nginx_error_log_update_time = os.path.getmtime('/var/log/nginx/error.log')
-    nginx_error_log_update_time = get_file_mtime(nginx_error_log_update_time)
-    nginx_error_log_size = os.path.getsize('/var/log/nginx/error.log')
-
-    logs.append(['blog.log', '程序运行日志!', app_log_update_time, basedir + '/logs/blogin.log', str(app_log_size) + 'byte'])
-    logs.append(['access.log', 'nginx access文件日志!', nginx_access_log_update_time, '/var/log/nginx/access.log',
-                 str(nginx_access_log_size) + 'byte'])
-    logs.append(['error.log', 'nginx error文件日志!', nginx_error_log_update_time, '/var/log/nginx/error.log',
-                 str(nginx_error_log_size) + 'byte'])
+    # logs.append(['access.log', 'nginx access文件日志!', nginx_access_log_update_time, '/var/log/nginx/access.log',
+    #              str(nginx_access_log_size) + 'byte'])
+    # logs.append(['error.log', 'nginx error文件日志!', nginx_error_log_update_time, '/var/log/nginx/error.log',
+    #              str(nginx_error_log_size) + 'byte'])
     return render_template('backend/logs.html', logs=logs)
+
+
+def get_log_file_info(app_log_path, logs, log_cate='程序运行日志！'):
+    for app_log in get_log_files(app_log_path):
+        app_log_update_time = get_file_mtime(app_log)
+        app_log_size = os.path.getsize(app_log)
+        logs.append(
+            [os.path.split(app_log)[1], log_cate, app_log_update_time, app_log, str(app_log_size) + 'byte'])
 
 
 @other_bp.route('/logs/detail/<path:file_path>/')
@@ -52,14 +60,24 @@ def log_detail(file_path):
         for line in f.readlines():
             line.strip('\n')
             if line.__contains__('[GET]') or line.__contains__('[POST]'):
-                line = "<span style='color: red;'>"+line+"</span>"
+                line = "<span style='color: red;'>" + line + "</span>"
             if line.__contains__('Traceback'):
-                line = "<span style='color: red;'>"+line+"</span>"
+                line = "<span style='color: red;'>" + line + "</span>"
             contents.append(line)
-    return render_template('backend/logDetail.html', contents=contents, path='/'+file_path)
+    return render_template('backend/logDetail.html', contents=contents, path='/' + file_path)
 
 
-def get_file_mtime(timestamp):
+def get_file_mtime(path):
+    timestamp = os.path.getmtime(path)
     timestamp = datetime.fromtimestamp(timestamp)
     timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
     return timestamp
+
+
+def get_log_files(path):
+    fl_ls = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if f.__contains__('log'):
+                fl_ls.append(os.path.join(root, f))
+    return fl_ls
