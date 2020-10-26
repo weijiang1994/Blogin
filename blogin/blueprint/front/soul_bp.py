53ab5d1a08bd630890c83a4c56284d32843528fd
jiangwei1 <jiangwei1@kylinos.com>
2020-10-26 10:51:55 +0800

    add poem

--- blogin/blueprint/front/soul_bp.py
+++ blogin/blueprint/front/soul_bp.py
@@ -6,10 +6,10 @@
 @File    : soul_bp
 @Software: PyCharm
 """
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template
 from sqlalchemy.sql.expression import func
 
-from blogin.models import Soul, Blog, SongCi
+from blogin.models import Soul, Blog, SongCi, Poem
 from blogin.utils import Lunar
 import datetime
 
@@ -22,29 +22,17 @@ def index():
     return render_template("main/soul.html", soul=soul)
 
 
-@soul_bp.route('/api/get_soul/')
-def get_soul():
-    counts = request.args.get('counts', 1)
-    if counts == 'all':
-        souls = Soul.query.all()
-    else:
-        souls = Soul.query.order_by(func.random()).limit(int(counts))
-    result = {'result': [], 'code': 200}
-    for soul in souls:
-        ls = {'title': soul.title, 'hits': soul.hits}
-        result.get('result').append(ls)
-    return jsonify(result)
-
-
-@soul_bp.route('/api/introduce/')
-def api_introduce():
-    blog = Blog.query.filter_by(title='API介绍').first()
-    return render_template("main/api/apiIntroduce.html", blog=blog)
-
-
 @soul_bp.route('/song-ci/')
 def song_ci():
     ci = SongCi.query.order_by(func.random()).limit(1)[0]
     now = datetime.date.today()
     lundar = Lunar(datetime.datetime.now())
     return render_template('main/poem/songCi.html', ci=ci, now=now, lundar=lundar)
+
+
+@soul_bp.route('/ts-poem/')
+def ts_poem():
+    poem = Poem.query.order_by(func.random()).limit(1)[0]
+    now = datetime.date.today()
+    lundar = Lunar(datetime.datetime.now())
+    return render_template('main/poem/tsPoem.html', poem=poem, now=now, lundar=lundar)
