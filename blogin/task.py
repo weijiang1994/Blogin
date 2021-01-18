41d1bd5d0c1ef9dfea44b9195d5ab47de4b6d72d
jiangwei1 <jiangwei1@kylinos.com>
2021-01-18 18:05:28 +0800

    todo

--- blogin/task.py
+++ blogin/task.py
@@ -7,6 +7,7 @@
 @Software: PyCharm
 """
 from blogin.models import Contribute, VisitStatistics, LikeStatistics, CommentStatistics
+from flask import current_app
 from blogin.extension import db, aps, rd
 import datetime
 from blogin.utils import github_social
@@ -65,3 +66,10 @@ def update_github_info():
         import traceback
         with open(basedir + '/logs/task.log', 'a') as f:
             f.write(traceback.print_exc())
+
+
+# 每月10号，20号自动更新百度AI平台secret key
+@aps.task('cron', id='update_baidu_ocr', day='10,20', hour='00', minute='00', second='50')
+def update_baidu_ocr():
+    with current_app.app_context():
+        pass
