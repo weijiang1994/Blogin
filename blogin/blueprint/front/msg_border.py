7d4a74bc9d4bac5eb9084622ca32fbba11e2cd87
weijiang1994 <804022023@qq.com>
2021-03-14 23:15:51 +0800

    feature: 实现留言功能

--- blogin/blueprint/front/msg_border.py
+++ blogin/blueprint/front/msg_border.py
@@ -7,12 +7,27 @@
 @Desc    : msg_border
 @Software: PyCharm
 """
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
+from blogin.models import MessageBorder
+from blogin.extension import db
 
 
-msg_border_bp = Blueprint('msg_border_bp', __name__, url_prefix='/msg_border/')
+msg_border_bp = Blueprint('msg_border_bp', __name__, url_prefix='/msg_border')
 
 
 @msg_border_bp.route('/')
 def index():
-    return render_template('main/message-border.html')
+    msg_borders = MessageBorder.query.filter_by(flag=0).order_by(MessageBorder.timestamps.desc()).all()
+
+    return render_template('main/message-border.html', msg_borders=msg_borders)
+
+
+@msg_border_bp.route('/leave_msg/', methods=['POST'])
+@login_required
+def leave_msg():
+    message = request.form.get('message')
+    mb = MessageBorder(user_id=current_user.id, body=message)
+    db.session.add(mb)
+    db.session.commit()
+    return '留言成功!'
