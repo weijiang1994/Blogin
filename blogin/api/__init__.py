f8e8f6cf6ac6d4707018f4acef2f71ac8f024196
weijiang1994 <804022023@qq.com>
2024-09-28 23:55:50 +0800

    featrue: 增加首页获取概览信息接口

--- blogin/api/__init__.py
+++ blogin/api/__init__.py
@@ -9,9 +9,11 @@ from flask import Flask
 from .frontend.auth import api_auth_bp
 from .frontend.index import index_bp
 from .backend.blog import blog_bp
+from .backend import api_index_bp
 
 
 def register_restful_api(app: Flask):
     app.register_blueprint(api_auth_bp)
     app.register_blueprint(index_bp)
     app.register_blueprint(blog_bp)
+    app.register_blueprint(api_index_bp)
--- blogin/api/backend/__init__.py
+++ blogin/api/backend/__init__.py
@@ -4,3 +4,28 @@ file: __init__.py.py
 @time: 2024/9/17 0:21
 @desc:
 """
+from flask import Blueprint
+from flask_jwt_extended import jwt_required
+
+from blogin.api.decorators import get_params
+from blogin.models import Blog, User, BlogComment, VisitStatistics
+from blogin.responses import R
+
+api_index_bp = Blueprint('api_index_bp', __name__, url_prefix='/api/index')
+
+
+@api_index_bp.route('/overview', methods=['GET'])
+@jwt_required
+def overview():
+    blog_count = Blog.query.count()
+    user_count = User.query.count()
+    comment_count = BlogComment.query.count()
+    visit_count = VisitStatistics.query.first().times
+    return R.success(
+        data=dict(
+            blog_count=blog_count,
+            user_count=user_count,
+            comment_count=comment_count,
+            visit_count=visit_count
+        )
+    )
